afaabaa8caf5a94e4d247adb34df0b2a4f8f9573
tamaohome <85826245+tamaohome@users.noreply.github.com>
2025-12-09 16:04:56 +0900

    📝 docs(utils): docstringを追加

--- matecon/utils.py
+++ matecon/utils.py
@@ -4,6 +4,13 @@ from pathlib import Path
 
 # TODO: 複数パスの入力に対応する
 def get_path_list(ext: str) -> list[Path]:
+    """
+    指定された拡張子のファイルパスを取得する
+
+    - コマンドライン引数から指定された拡張子のファイルパスを取得する
+    - コマンドライン引数に該当するファイルがない場合は、ファイルパスの標準入力を求める
+    """
+
     def _get_paths_from_argv():
         paths = [Path(p.replace('"', "")) for p in sys.argv[1:]]
         return _validated_paths(paths)
@@ -31,6 +38,13 @@ def get_path_list(ext: str) -> list[Path]:
 
 
 def write_txt(org_path: Path | str, lines: list[str]) -> Path:
+    """
+    指定されたファイルパスに文字列リストを書き出す
+
+    - ファイルパスは `.txt` に変換される
+    - エンコードは shift-jis (cp932) 固定
+    """
+
     txt_path = Path(org_path).resolve().with_suffix(".txt")
     with txt_path.open(mode="w", encoding="cp932") as f:
         for line in lines:
