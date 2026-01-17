c6d3cffa2e0f8ab0867c79654c9f8b651e696d0e
tamaohome <85826245+tamaohome@users.noreply.github.com>
2026-01-17 12:45:42 +0900

    🧹 chore: cuiの実装を一時的に削除

--- matecon/__main__.py
+++ matecon/__main__.py
@@ -3,32 +3,18 @@ import warnings
 
 from PySide6.QtWidgets import QApplication
 
-from matecon.gui.controller import Controller
 from matecon.gui.view import MainWindow
-from matecon.io.io import get_path_list
 
 # openpyxl の警告を無視
 warnings.filterwarnings("ignore", category=UserWarning, module="openpyxl")
 
 
-def gui_app():
+def app():
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
 
 
-def cui_app():
-    xlsx_files = get_path_list(".xlsx")
-    controller = Controller()
-    try:
-        controller.convert_file(str(xlsx_files[0]))
-    finally:
-        controller.cleanup()
-
-
 if __name__ == "__main__":
-    if "--cui" in sys.argv:
-        cui_app()
-    else:
-        gui_app()
+    app()
