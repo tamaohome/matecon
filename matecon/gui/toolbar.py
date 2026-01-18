c3e17d31dd56bce8dbdd2e19d238c48dbe208b0b
tamaohome <85826245+tamaohome@users.noreply.github.com>
2026-01-18 09:56:57 +0900

    ✨ feat(gui): ツールバーにアイコンを追加

--- matecon/gui/toolbar.py
+++ matecon/gui/toolbar.py
@@ -1,6 +1,6 @@
-from PySide6.QtCore import Signal
+from PySide6.QtCore import QSize, Signal
 from PySide6.QtGui import QAction
-from PySide6.QtWidgets import QToolBar
+from PySide6.QtWidgets import QStyle, QToolBar
 
 
 class MainToolBar(QToolBar):
@@ -19,18 +19,31 @@ class MainToolBar(QToolBar):
 
     def _setup_actions(self):
         """ツールバーアクションを設定"""
+
+        # アイコンの横にラベルを表示
+        self.setToolButtonStyle(self.toolButtonStyle().ToolButtonTextBesideIcon)
+        # デフォルトのアイコンサイズを設定
+        default_icon_size = QSize(16, 16)
+        self.setIconSize(default_icon_size)
+
         self.action_open = QAction("ファイルを開く", self)
         self.action_open.setShortcut("Ctrl+O")
         self.action_open.triggered.connect(self.addFileTriggered.emit)
+        icon_open = self.style().standardIcon(QStyle.StandardPixmap.SP_DialogOpenButton)
+        self.action_open.setIcon(icon_open)
         self.addAction(self.action_open)
 
         self.action_convert = QAction("テキストに変換", self)
         self.action_convert.setShortcut("Ctrl+E")
         self.action_convert.setEnabled(False)  # 初期状態は無効
         self.action_convert.triggered.connect(self.convertTriggered.emit)
+        icon_save = self.style().standardIcon(QStyle.StandardPixmap.SP_DialogSaveButton)
+        self.action_convert.setIcon(icon_save)
         self.addAction(self.action_convert)
 
         self.action_clear = QAction("ファイル一覧をクリア", self)
         self.action_clear.setEnabled(False)  # 初期状態は無効
         self.action_clear.triggered.connect(self.clearTriggered.emit)
+        icon_clear = self.style().standardIcon(QStyle.StandardPixmap.SP_LineEditClearButton)
+        self.action_clear.setIcon(icon_clear)
         self.addAction(self.action_clear)
