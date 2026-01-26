084bc4be75ac2ee83bbfedf5d2ac215edc5d1881
tamaohome <85826245+tamaohome@users.noreply.github.com>
2026-01-27 00:25:41 +0900

    ✨ feat(models): クラス名を変更 (PathSet->ExcelFileSet)

---
+++ matecon/models/excel_file_set.py
@@ -0,0 +1,60 @@
+from __future__ import annotations
+
+from collections.abc import Iterable, Iterator, MutableSet
+from contextlib import suppress
+
+from boltons.setutils import IndexedSet
+
+from matecon.models.excel_file import ExcelFile
+
+
+class ExcelFileSet(MutableSet[ExcelFile]):
+    """
+    ファイルパスを `ExcelFile` オブジェクトとして保持する順序付き集合クラス
+
+    - `in` 演算子で中身を調べるときは `ExcelFile` を指定する
+    """
+
+    def __init__(self, excel_files: Iterable[ExcelFile] = []) -> None:
+        self._inner: IndexedSet = IndexedSet()
+        self.add(*excel_files)
+
+    def add(self, *excel_files: ExcelFile) -> None:
+        """`ExcelFile` オブジェクトを追加する"""
+        for excel_file in excel_files:
+            self._inner.add(excel_file)
+
+    def discard(self, excel_file: ExcelFile) -> None:
+        """`
+        ExcelFile` オブジェクトを削除する
+
+        Excelファイルが存在しない場合はエラーを発生せず処理を中止する
+        """
+        with suppress(KeyError):
+            self._inner.remove(excel_file)
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({list(self._inner)})"
+
+    def __len__(self) -> int:
+        return len(self._inner)
+
+    def __getitem__(self, index: int) -> ExcelFile:
+        return self._inner[index]
+
+    def __iter__(self) -> Iterator[ExcelFile]:
+        return iter(self._inner)
+
+    def __contains__(self, excel_file: ExcelFile) -> bool:
+        return excel_file in self._inner
+
+    def __add__(self, other: ExcelFile | ExcelFileSet) -> ExcelFileSet:
+        """加算演算子で `ExcelFile | ExcelFileSet` を追加した新しいセットを返す"""
+        new_set = ExcelFileSet(self)
+        if isinstance(other, ExcelFile):
+            new_set.add(other)
+        elif isinstance(other, ExcelFileSet):
+            [new_set.add(f) for f in other]
+            for excel_file in other:
+                new_set.add(excel_file)
+        return new_set
